fd1f7aef13636a35d65311dbf586910503d544c2
Sebastian Safin <sebastian.safin1@gmail.com>
2018-08-26 14:33:24 +0200

    Added GetNextState method to Cell class

---
+++ GameOfLifeLibrary/Cell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeLibrary
+{
+    public class Cell
+    {
+        private CellState state;
+
+        public enum CellState
+        {
+            Alive,
+            Dead
+        }
+
+        public Cell(CellState state)
+        {
+            this.state = state;
+        }
+
+        public CellState GetNextState(int numberOfNeighbours)
+        {
+            if (state == CellState.Alive)
+                return numberOfNeighbours == 2 || numberOfNeighbours == 3 ? CellState.Alive : CellState.Dead;
+            else
+                return numberOfNeighbours == 3 ? CellState.Alive : CellState.Dead;
+        }
+    }
+}
---
+++ GameOfLifeLibraryTests/CellTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xunit;
+using GameOfLifeLibrary;
+using static GameOfLifeLibrary.Cell;
+using System.Collections.Generic;
+
+namespace GameOfLifeLibraryTest
+{
+    public class CellTest
+    {
+        public class GetNextStateMethod
+        {
+            public static IEnumerable<object[]> AliveTestData =
+                new List<object[]>
+                {
+                    new object[] {0, CellState.Dead},
+                    new object[] {1, CellState.Dead},
+                    new object[] {2, CellState.Alive},
+                    new object[] {3, CellState.Alive},
+                    new object[] {4, CellState.Dead},
+                    new object[] {5, CellState.Dead},
+                    new object[] {6, CellState.Dead},
+                    new object[] {7, CellState.Dead},
+                    new object[] {8, CellState.Dead}
+                };
+
+            public static IEnumerable<object[]> DeadTestData =
+                new List<object[]>
+                {
+                    new object[] {0, CellState.Dead},
+                    new object[] {1, CellState.Dead},
+                    new object[] {2, CellState.Dead},
+                    new object[] {3, CellState.Alive},
+                    new object[] {4, CellState.Dead},
+                    new object[] {5, CellState.Dead},
+                    new object[] {6, CellState.Dead},
+                    new object[] {7, CellState.Dead},
+                    new object[] {8, CellState.Dead}
+                 };
+
+            [Theory]
+            [MemberData(nameof(AliveTestData))]
+            public void ShouldFullfillTransitionWhenAlive(int numberOfNeighbours, CellState expected)
+            {
+                Cell cell = new Cell(CellState.Alive);
+
+                CellState actual = cell.GetNextState(numberOfNeighbours);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Theory]
+            [MemberData(nameof(DeadTestData))]
+            public void ShouldFullfillTransitionWhenDead(int numberOfNeighbours, CellState expected)
+            {
+                Cell cell = new Cell(CellState.Dead);
+
+                CellState actual = cell.GetNextState(numberOfNeighbours);
+
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
